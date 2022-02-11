12db251d2243a740d9f03ba0e45511851cb7b2c6
indhumathi <Indhumathi.Ramesh@disys.com>
2022-02-11 07:06:23 -0800

    Add project files.

---
+++ Compile time polymorphism/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Calculation
+{
+    public static int AddNumbers(int numberOne, int numberTwo)
+    {
+        return numberOne + numberTwo;
+    }
+    public static float AddNumbers(float numberOne, float numberTwo)
+    {
+        return numberOne + numberTwo;
+    }
+}
+public class Overloading
+{
+    public static void Main()
+    {
+        Console.WriteLine(Calculation.AddNumbers(11, 12));
+        Console.WriteLine(Calculation.AddNumbers(11.4f, 12.3f));
+    }
+}
+
